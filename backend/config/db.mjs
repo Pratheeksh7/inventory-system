a66eb19b643efb4859638ff393900f861fce0615
Pratheeksh <pratheeksh.729@gmail.com>
2025-02-01 19:36:26 +0530

    Final commit(password)

--- backend/config/db.mjs
+++ backend/config/db.mjs
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const connectDB = mysql.createConnection({
   host:'localhost',
   user:'root',
-  password:'Pratheeksh@729',
+  password:'',
   database:'InventoryDB'
 });
 
